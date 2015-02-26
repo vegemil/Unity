548e60ffc5f9dbec695dbdb600f53a9bd6e0b31e
vegemil <euna501@gmail.com>
2015-02-27 00:14:55 +0900

    BoxRunner
    효과음할차례

---
+++ BoxRunner/Assets/Script/Player_Ctrl.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerState
+{
+	RUN,
+	JUMP,
+	DOUBLEJUMP,
+	DEATH
+};
+
+public class Player_Ctrl : MonoBehaviour {
+
+	public PlayerState PState;
+	public float Jump_Power = 500f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if (PState == PlayerState.JUMP)
+				DoubleJump();
+
+			if(PState == PlayerState.RUN)
+				Jump();
+
+			
+		}
+	}
+
+	void Jump()
+	{
+		PState = PlayerState.JUMP;
+		rigidbody.AddForce(new Vector3(0, Jump_Power, 0));
+	}
+
+	void DoubleJump()
+	{
+		PState = PlayerState.DOUBLEJUMP;
+		rigidbody.AddForce(new Vector3(0, Jump_Power, 0));
+	}
+
+	void Run()
+	{
+		PState = PlayerState.RUN;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(PState != PlayerState.RUN)
+		{
+			Run();
+		}
+	}
+
+	void GetCoin()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		rigidbody.WakeUp();
+		if(other.gameObject.name == "Coin")
+		{
+			Destroy(other.gameObject);
+			GetCoin();
+		}
+
+		if (other.gameObject.name == "DeathZone" && PState != PlayerState.DEATH)
+		{
+			GameOver();
+		}
+	}
+
+	void GameOver()
+	{
+		PState = PlayerState.DEATH;
+	}
+}
