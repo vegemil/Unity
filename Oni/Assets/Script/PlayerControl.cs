90fd322f7e301c047ba6d00dd8614ae24aefa3dc
vegemil <euna501@gmail.com>
2015-05-14 16:27:49 +0900

    7초마다 말 머리가 나오도록 수정

--- Oni/Assets/Script/PlayerControl.cs
+++ Oni/Assets/Script/PlayerControl.cs
@@ -6,6 +6,7 @@ public class PlayerControl : MonoBehaviour {
 	public float Speed = 1.0f;
 	public GameObject[] horses;
 
+	float time = 0;
 	public enum Status
 	{
 		IDLE,
@@ -22,24 +23,35 @@ public class PlayerControl : MonoBehaviour {
 	void Update () {
 		this.transform.Translate(Vector3.forward * Time.deltaTime * Speed);
 
-		StartCoroutine("RezenHorse");
+		time += Time.deltaTime;
+
+        Debug.Log(time);
+
+		if(time >=7)
+		{
+			StartCoroutine("RezenHorse");
+			time = 0;
+		}
 	}
 
 	//말머리 만듬
 	IEnumerator RezenHorse()
 	{
-		yield return new WaitForSeconds(Random.Range(5, 10));
+// 		yield return new WaitForSeconds(5);
 
 		for(int i = 0; i<horses.Length; i++)
 		{
 			if(horses[i].activeInHierarchy == false)
 			{
-				horses[i].transform.position = new Vector3(0, 1.4f,  + FindHorse().transform.position.z + Random.Range(5, 20));
+// 				horses[i].transform.position = new Vector3(0, 1.4f,  + FindHorse().transform.position.z + Random.Range(5, 20));
+				horses[i].transform.position = new Vector3(0, 1.4f, this.gameObject.transform.position.z + Random.Range(5, 20));
 				horses[i].SetActive(true);
 
 				break;
 			}
 		}
+
+        yield return null;
 	}
 
 	//플레이어로 부터 가장 멀리 있는 말머리를 찾음
