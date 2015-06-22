37caa53cef7201fe39bc5fabb7848c71cff87e3c
Vegemil <euna501@gmail.com>
2015-06-22 23:34:27 +0900

    Player에 rigidbody 붙여서 움직임 칼 먹기, 다이아몬드 먹기 가능
    버섯 AI만들 차례

--- Dungeon Eater/Assets/Script/Map.cs
+++ Dungeon Eater/Assets/Script/Map.cs
@@ -3,104 +3,122 @@ using System.Collections;
 
 public class Map : MonoBehaviour {
 
-    public TextAsset map_textAsset;
-    string[] maptext;
+	public TextAsset map_textAsset;
+	string[] maptext;
 
-    int width;
-    int height;
+	int width;
+	int height;
 
-    char[,] mapdata;
+	char[,] mapdata;
 
-    public GameObject jewel;
-    public GameObject block;
-    public GameObject player;
-    public GameObject ghost;
-    public GameObject sword;
-    public GameObject empty;
-    GameObject Planes;
+	public GameObject jewel;
+	public GameObject block;
+	public GameObject player;
+	public GameObject ghost;
+	public GameObject sword;
+	public GameObject empty;
+	GameObject Planes;
+	GameObject Jewels;
+	GameObject Blocks;
 
-    enum MAPFORMAT
-    {
-        JEWEL,
-        BLOCK,
-        PLAYER,
-        GHOST,
-        SWORD,
-        EMPTY
-    };
+
+	enum MAPFORMAT
+	{
+		JEWEL,
+		BLOCK,
+		PLAYER,
+		GHOST,
+		SWORD,
+		EMPTY
+	};
 
 	// Use this for initialization
 	void Start () {
-        Planes = new GameObject();
-        Planes.transform.position = new Vector3(0, 0, 0);
+		Planes = new GameObject();
+		Planes.transform.position = new Vector3(0, 0, 0);
+		Planes.transform.name = "Planes";
 
-        System.StringSplitOptions option = System.StringSplitOptions.RemoveEmptyEntries;
+		Jewels = new GameObject();
+        Jewels.transform.position = new Vector3(0, 0.224f, 0);
+		Jewels.transform.name = "Jewels";
 
-        maptext = map_textAsset.text.Split(new char[] {'\r', '\n'}, option);
+		Blocks = new GameObject();
+		Blocks.transform.position = new Vector3(0, 0, 0);
+		Blocks.transform.name = "Blocks";
 
-        char[] spliter = new char[1] { ',' };
+		System.StringSplitOptions option = System.StringSplitOptions.RemoveEmptyEntries;
 
-        string[] sizewh = maptext[0].Split(spliter, option);
-        width = int.Parse(sizewh[0]);
-        height = int.Parse(sizewh[1]);
+		maptext = map_textAsset.text.Split(new char[] {'\r', '\n'}, option);
 
-        mapdata = new char[height, width];
+		char[] spliter = new char[1] { ',' };
 
-        for (int i = 0; i<height; ++i)
-        {
-            string[] data = maptext[height - i].Split(spliter, option);
+		string[] sizewh = maptext[0].Split(spliter, option);
+		width = int.Parse(sizewh[0]);
+		height = int.Parse(sizewh[1]);
 
-            for(int j = 0; j<width; ++j)
-            {
-                mapdata[i, j] = data[j][0];
-            }
-        }
+		mapdata = new char[height, width];
 
-        CreateMap();
+		for (int i = 0; i<height; ++i)
+		{
+			string[] data = maptext[height - i].Split(spliter, option);
 
-        
-    }
+			for(int j = 0; j<width; ++j)
+			{
+				mapdata[i, j] = data[j][0];
+			}
+		}
+
+		CreateMap();
+
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-    void CreateMap()
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            for (int z = 0; z < height; ++z)
-            {
-                GameObject obj = Instantiate(SwapData(mapdata[z, x]), new Vector3(x, 0, z), Quaternion.identity) as GameObject;
+	void CreateMap()
+	{
+		for (int x = 0; x < width; ++x)
+		{
+			for (int z = 0; z < height; ++z)
+			{
+				GameObject obj = Instantiate(SwapData(mapdata[z, x]), new Vector3(x, 0, z), Quaternion.identity) as GameObject;
 
-                GameObject plane = Instantiate(empty, new Vector3(x, 0, z), Quaternion.Euler(new Vector3(90, 0, 0))) as GameObject;
-                plane.transform.parent = Planes.gameObject.transform;
-            }
-        }
-    }
+				if (obj.transform.name.Contains("Jewel"))
+					obj.transform.parent = Jewels.gameObject.transform;
+				else if (obj.transform.name.Contains("Cube"))
+					obj.transform.parent = Blocks.gameObject.transform;
 
-    GameObject SwapData(char data)
-    {
-        switch(data)
-        {
-            case 'c':
-            case 't':
-                return jewel;
-            case '*':
-                return block;
-            case 'p':
-                return player;
-            case 's':
-                return sword;
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-                return ghost;
-            case 'x':
-                return empty;
-        }
-        return null;
-    }
+
+				GameObject plane = Instantiate(empty, new Vector3(x, 0, z), Quaternion.Euler(new Vector3(90, 0, 0))) as GameObject;
+				plane.transform.parent = Planes.gameObject.transform;
+			}
+		}
+	}
+
+	GameObject SwapData(char data)
+	{
+		switch(data)
+		{
+			case 'c':
+			case 't':
+				return jewel;
+			case '*':
+				return block;
+			case 'p':
+				return player;
+			case 's':
+				return sword;
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+				return ghost;
+			case 'x':
+				return empty;
+		}
+		return null;
+	}
 }
---
+++ Dungeon Eater/Assets/Script/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	Text jewelText;
+	Text swordText;
+
+	int count = 0;
+	bool ItHasSword = false;
+
+	// Use this for initialization
+	void Start () {
+		jewelText = GameObject.Find("Jewel Count Text").GetComponent<Text>();
+		swordText = GameObject.Find("Sword Text").GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (ItHasSword == true)
+		{
+			swordText.text = "칼 있음!!";
+		}
+		else
+			swordText.text = "칼 없음!!";
+	}
+
+	// OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.transform.name.Contains("Jewel"))
+		{
+			count++;
+			jewelText.text = count.ToString() + "개";
+
+			Destroy(collision.gameObject);
+		}
+
+		else if (collision.gameObject.transform.name.Contains("Sword"))
+		{
+			ItHasSword = true;
+		}
+
+	}
+
+
+}
