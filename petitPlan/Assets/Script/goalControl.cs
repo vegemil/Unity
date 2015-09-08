19c81b323cb8e8209e2c5e4d4c0c687da5b03c8c
Vegemil <euna501@gmail.com>
2015-09-09 01:39:26 +0900

    반다이남코 디렉터가 알려주는 유니티 게임 제작 입문 4장

---
+++ petitPlan/Assets/Script/goalControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class goalControl : MonoBehaviour {
+
+    private bool is_collided = false;
+    public Text text;
+
+    public float GOAL_MIN = 5.0f;
+    public float GOAL_MAX = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        float rnd = Random.Range(GOAL_MIN, GOAL_MAX);
+
+        this.transform.position = new Vector3(rnd, -1.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(is_collided == true)
+        {
+            text.gameObject.SetActive(true);
+        }
+	}
+
+    public void OnCollisionStay(Collision collision)
+    {
+        is_collided = true;
+    }
+
+
+}
---
+++ petitPlan/Assets/Script/playerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+
+	private float power;
+	public float POWERPLUS = 100.0f;
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{
+			power += POWERPLUS * Time.deltaTime;
+		}
+
+		if(Input.GetMouseButtonUp(0))
+		{
+			rigidBody.AddForce(new Vector3(power, power, 0));
+			power = 0;
+		}
+
+		if(this.transform.position.y < -5f)
+		{
+			Application.LoadLevel("gameScene");
+		}
+	}
+}
