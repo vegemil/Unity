83fc3ada98755c22b70646d830ac1fe872d95ee5
Vegemil <euna501@gmail.com>
2015-11-19 23:17:36 +0900

    드랍박스에서 선택시 이미지 변경

---
+++ Vilage/Assets/Script/DropBoxClick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DropBoxClick : MonoBehaviour {
+
+    public Dropdown dropdown;
+    private Image image;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+        dropdown.onValueChanged.AddListener(DropdownValueChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DropdownValueChange(int value)
+    {
+        image.sprite = dropdown.options[value].image;
+        Debug.Log(dropdown.options[value].image.name);
+    }
+}
