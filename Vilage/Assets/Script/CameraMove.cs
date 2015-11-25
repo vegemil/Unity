038643bc2417f0b24842391c17e285ce36ef8919
Vegemil <euna501@gmail.com>
2015-11-25 19:47:59 +0900

    클릭 카메라 컨트롤
    왼쪽 누르면 position이동
    오른쪽 누르면 rotation 회전

--- Vilage/Assets/Script/CameraMove.cs
+++ Vilage/Assets/Script/CameraMove.cs
@@ -8,6 +8,7 @@ public class CameraMove : MonoBehaviour
     private Vector3 delta;
 
     public float rotateSpeed = 20;
+    public float moveSpeed = 10;
 
     public float horizontalSpeed = 2.0F;
     public float verticalSpeed = 2.0F;
@@ -28,42 +29,31 @@ public class CameraMove : MonoBehaviour
         }
         else if (Input.GetMouseButton(1))
         {
-            delta = Input.mousePosition - lastPosition;
-
-            Debug.Log("delta X : " + delta.x);
-            Debug.Log("delta Y : " + delta.y);
-
-            //transform.RotateAround(lastPosition, delta, Time.deltaTime * rotateSpeed);
-            //transform.Rotate(delta * 0.01f);
-
             float h = horizontalSpeed * Input.GetAxis("Mouse X");
             float v = verticalSpeed * Input.GetAxis("Mouse Y");
 
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            transform.LookAt(transform.position);
 
-            if(Physics.Raycast(ray, out hit, 100f))
-            {
-                GameObject parent = hit.transform.parent.gameObject;
-
-                while (parent.transform.parent.gameObject.tag != "Ground")
-                {
-                    parent = parent.transform.parent.gameObject;
-                }
-
-                target = parent.transform.parent.position; 
-            }
-
-
-            transform.LookAt(target);
-
-            //-1f를 곱해 마우스가 움직이는 방향대로 움직이도록 함
             transform.RotateAround(target, new Vector3(v * -1f, h * -1f, 0), Time.deltaTime * rotateSpeed);
 
             lastPosition = Input.mousePosition;
         }
 
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            delta = Input.mousePosition - lastPosition;
+
+            Debug.Log("delta X : " + delta.x);
+            Debug.Log("delta Y : " + delta.y);
+
+            transform.Translate(delta * Time.deltaTime * moveSpeed);
+
+            lastPosition = Input.mousePosition;
+        }
 
     }
 
